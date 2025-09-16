5725f93aaf32fda02e01342a14bc4e7dd1878379
root <root@LAPTOP-HNMFBH9L.localdomain>
2025-09-16 23:40:24 +0500

    Fix API URL to use Elastic Beanstalk backend directly

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,10 +1,7 @@
 // API Configuration
 // For production, set VITE_API_URL environment variable in AWS Amplify
 
-export const API_URL = import.meta.env.VITE_API_URL ||
-  (import.meta.env.PROD
-    ? 'http://area25-simple.eba-b42mgv5j.eu-north-1.elasticbeanstalk.com/api'
-    : '/api'
-  );
+// Force production URL since we're deploying to Amplify
+export const API_URL = 'http://area25-simple.eba-b42mgv5j.eu-north-1.elasticbeanstalk.com/api';
 
 export const API_BASE = API_URL;
